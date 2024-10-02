d4e4f7415f1ab788b8fa605bd651679ace5d5a57
vasiliy-muravev <vasiliymuravev@yandex.ru>
2024-10-02 16:53:49 +0300

    Контейнеры и CI/CD для Kittygram

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.252.141.216', '127.0.0.1', 'localhost', 'vasiliymuravev.servehttp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -92,6 +92,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
